4d2b08ffba08662295783db0cdacb00e6702f76c
Ruben Costa <costamartinezruben@gmail.com>
2020-08-02 21:13:02 +0100

    initial trading layout

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
-import InfoBar from '../components/InfoBar'
+import InfoBar from './InfoBar';
 
 export default function Home(): JSX.Element {
   return (
@@ -10,29 +10,22 @@ export default function Home(): JSX.Element {
       <div className={styles.container} data-tid="container">
         <div className={styles.innerContainer}>
           <div className={styles.logoBox}>
-            <h2>RA TRADING</h2> <span className={styles.proLabel}>pro</span>
+            <h2>RA TRADING</h2>
+            <span className={styles.proLabel}>pro</span>
           </div>
           <Link to={routes.COUNTER}>Trades</Link>
         </div>
       </div>
       <div className={styles.main}>
         <div className={styles.topMain}>
-          <InfoBar />
+          <InfoBar/>
         </div>
           <div className={styles.downMain}>
             <div className={styles.mainLayout}>
-              <div className={styles.sidebar}>
-                Sidebar
-              </div>
-              <div className={styles.orderBookTabbed}>
-                Order Book
-              </div>
-              <div className={styles.chart}>
-                Chart
-              </div>
-              <div className={styles.trading}>
-                Trading
-              </div>
+              <div className={styles.sidebar}>Sidebar</div>
+              <div className={styles.orderBookTabbed}>Order Book</div>
+              <div className={styles.chart}>Chart</div>
+              <div className={styles.trading}>Trading</div>
             </div>
           </div>
       </div>
