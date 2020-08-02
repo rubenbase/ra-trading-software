03b6a92181fb2657bf40a1f50b99dab9dea39765
Ruben Costa <costamartinezruben@gmail.com>
2020-08-02 23:36:31 +0100

    start info bar component

--- app/components/InfoBar/InfoBar.tsx
+++ app/components/InfoBar/InfoBar.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-// import styles from './InfoBar.css';
+import styles from './InfoBar.css';
+import { GrGoogleWallet } from 'react-icons/gr';
 
 export default function InfoBar(): JSX.Element {
   return (
-    <>
-    Info Bar is here
-    </>
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <div className={styles.totalPortfolioBox}>
+          <GrGoogleWallet size={16} />
+          <span className={styles.totalPortfolio}>£34,000</span>
+        </div>
+        <div>
+          <span className={styles.totalPortfolio}>ETH - £400</span><span className={styles.totalPortfolio}>BTC - 11000</span>
+        </div>
+      </div>
+    </div>
   );
 }
