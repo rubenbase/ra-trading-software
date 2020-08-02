28e69c7be74f20d97022562ae941cdd87f977d29
Ruben Costa <costamartinezruben@gmail.com>
2020-08-02 21:12:06 +0100

    add trading layout

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,12 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
+import InfoBar from '../components/InfoBar'
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
-    </div>
+    <>
+      <div className={styles.container} data-tid="container">
+        <div className={styles.innerContainer}>
+          <div className={styles.logoBox}>
+            <h2>RA TRADING</h2> <span className={styles.proLabel}>pro</span>
+          </div>
+          <Link to={routes.COUNTER}>Trades</Link>
+        </div>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.topMain}>
+          <InfoBar />
+        </div>
+          <div className={styles.downMain}>
+            <div className={styles.mainLayout}>
+              <div className={styles.sidebar}>
+                Sidebar
+              </div>
+              <div className={styles.orderBookTabbed}>
+                Order Book
+              </div>
+              <div className={styles.chart}>
+                Chart
+              </div>
+              <div className={styles.trading}>
+                Trading
+              </div>
+            </div>
+          </div>
+      </div>
+    </>
   );
 }
---
+++ app/components/InfoBar/InfoBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import styles from './InfoBar.css';
+
+export default function InfoBar(): JSX.Element {
+  return (
+    <>
+    Info Bar is here
+    </>
+  );
+}
