238481f86ac523014568f15c51e98c466d930fa2
Ruben Costa <costamartinezruben@gmail.com>
2020-08-03 02:48:41 +0100

    wip completee navbar structure

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -4,30 +4,54 @@ import routes from '../constants/routes.json';
 import styles from './Home.css';
 import InfoBar from './InfoBar';
 
+import {MdApps} from 'react-icons/md'
+import {FaChevronDown} from 'react-icons/fa'
 export default function Home(): JSX.Element {
   return (
     <>
       <div className={styles.container} data-tid="container">
         <div className={styles.innerContainer}>
+            <div className={styles.leftMenuBox}>
           <div className={styles.logoBox}>
             <h2>RA TRADING</h2>
             <span className={styles.proLabel}>pro</span>
           </div>
-          <Link to={routes.COUNTER}>Trades</Link>
+            <div className={styles.leftMenuIcon}>
+
+              <div>
+              <MdApps size={24}/>
+              </div>
+              <div>
+              <FaChevronDown size={8}/>
+              </div>
+            </div>
+
+            </div>
+          <div className={styles.menuBox}>
+            <div className={styles.menuLink}>
+              <Link to={routes.COUNTER}>GBP</Link>
+            </div>
+            <div className={styles.menuLink}>
+              <Link to={routes.COUNTER}>English</Link>
+            </div>
+            <div className={styles.menuLink}>
+              <Link to={routes.COUNTER}>Ruben C.</Link>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.main}>
         <div className={styles.topMain}>
           <InfoBar/>
         </div>
-          <div className={styles.downMain}>
-            <div className={styles.mainLayout}>
-              <div className={styles.sidebar}>Sidebar</div>
-              <div className={styles.orderBookTabbed}>Order Book</div>
-              <div className={styles.chart}>Chart</div>
-              <div className={styles.trading}>Trading</div>
-            </div>
+        <div className={styles.downMain}>
+          <div className={styles.mainLayout}>
+            <div className={styles.sidebar}>Sidebar</div>
+            <div className={styles.orderBookTabbed}>Order Book</div>
+            <div className={styles.chart}>Chart</div>
+            <div className={styles.trading}>Trading</div>
           </div>
+        </div>
       </div>
     </>
   );
--- app/components/InfoBar/InfoBar.tsx
+++ app/components/InfoBar/InfoBar.tsx
@@ -2,16 +2,93 @@ import React from 'react';
 import styles from './InfoBar.css';
 import { GrGoogleWallet } from 'react-icons/gr';
 
+const stocks = [
+  {
+    pair: 'bitcoin',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '+0.4343',
+    positive: true,
+  },
+  {
+    pair: 'etherium',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '+0.4343',
+    positive: false,
+  },
+  {
+    pair: 'ripple',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '+0.4343',
+    positive: false,
+  },
+  {
+    pair: 'ample',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '+0.4343',
+    positive: true,
+  },
+
+  {
+    pair: 'PEPE',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '3',
+    positive: false,
+  },
+  {
+    pair: 'MARAI',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '3',
+    positive: false,
+  },
+  {
+    pair: 'JOSE',
+    price: '11,400',
+    change24: '0.423',
+    percentage24: '3',
+    positive: false,
+  },
+]
+
 export default function InfoBar(): JSX.Element {
+  console.log(stocks)
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
         <div className={styles.totalPortfolioBox}>
           <GrGoogleWallet size={16} />
-          <span className={styles.totalPortfolio}>£34,000</span>
+          <span className={styles.totalPortfolio}>£38,000</span>
         </div>
-        <div>
-          <span className={styles.totalPortfolio}>ETH - £400</span><span className={styles.totalPortfolio}>BTC - 11000</span>
+        <div className={styles.stockCarrousel}>
+          <ul className={styles.stockCarrouselList}>
+            {stocks.map((stock) => (
+                <li className={styles.stockCarrouselUnit}>
+                  <div className={`${styles.stockPercentage} ${styles.error}`}>
+                    <span className={styles.stockPair}>{stock.pair}&nbsp;{"·"}</span>
+                    <span className={`${styles.stockChange24} ${stock.positive ? styles.success : styles.error}`}>{stock.change24}</span>
+                    <span className={`${styles.stockPercentage24} ${stock.positive ? styles.success : styles.error}`}>({stock.percentage24})</span>
+                  </div>
+                  <span className={styles.stockPrice}>{stock.price}</span>
+                </li>
+            ))}
+          </ul>
+          <ul className={styles.stockCarrouselList}>
+            {stocks.map((stock) => (
+                <li className={styles.stockCarrouselUnit}>
+                  <div className={`${styles.stockPercentage} ${styles.error}`}>
+                    <span className={styles.stockPair}>{stock.pair}&nbsp;{"·"}</span>
+                    <span className={`${styles.stockChange24} ${stock.positive ? styles.success : styles.error}`}>{stock.change24}</span>
+                    <span className={`${styles.stockPercentage24} ${stock.positive ? styles.success : styles.error}`}>({stock.percentage24})</span>
+                  </div>
+                  <span className={styles.stockPrice}>{stock.price}</span>
+                </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
